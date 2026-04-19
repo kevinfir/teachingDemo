3730016b0a35f40b44540516ba6d7c8e41fd3c5f
kevinfir <95353256+kevinfir@users.noreply.github.com>
2026-04-19 12:32:39 +0800

    Refactor page.py by removing media route
    Removed media route and unnecessary imports.

--- page.py
+++ page.py
@@ -1,22 +1,12 @@
-from pathlib import Path
-
-from flask import Flask, abort, render_template, send_from_directory
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
-BASE_DIR = Path(__file__).resolve().parent
 
 
 @app.route("/")
 def index():
 	return render_template("index.html")
-
-
-@app.route("https://youtu.be/UZUXVAkE2Os")
-def media(filename):
-	if filename != "crawler.mp4":
-		abort(404)
-	return send_from_directory(BASE_DIR, filename)
 
 
 if __name__ == "__main__":
